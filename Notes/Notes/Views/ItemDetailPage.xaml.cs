f0de0e6ec5ea2d6aac706139a57eaf50c7aab0a6
Thais Oliveira <84998357+Never1Surrender@users.noreply.github.com>
2022-02-10 12:19:54 -0300

    Initial commit

---
+++ Notes/Notes/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Notes.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Notes.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ Notes/Notes/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Notes.Models;
+using Notes.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Notes.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
